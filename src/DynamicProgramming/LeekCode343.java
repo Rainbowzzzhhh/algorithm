9daea7ed09e293dab8b4b7818d28ae5f8168f9e7
rainbow <562783546@qq.com>
2025-02-12 15:00:46 +0800

    Initial commit

---
+++ src/DynamicProgramming/LeekCode343.java
@@ -0,0 +1,13 @@
+package DynamicProgramming;
+
+/**
+ * @author rainbow
+ * @time 2025-02-12 14:51
+ * @description ...
+ */
+public class LeekCode343 {
+    public int integerBreak(int n) {
+
+    }
+
+}
