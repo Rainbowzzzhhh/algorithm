c9407444dd21ddf29b18d86842b8f694f54acfc7
rainbow <562783546@qq.com>
2025-02-12 23:40:08 +0800

    tree

--- src/Tree/Front.java
+++ src/Tree/Front.java
@@ -1,6 +1,7 @@
 package Tree;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Stack;
 
@@ -53,6 +54,27 @@ class Traversal {
                 cur = cur.right;
             }
         }
+        return result;
+    }
+
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            result.add(node.val);
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+        }
+        Collections.reverse(result);
         return result;
     }
 }
