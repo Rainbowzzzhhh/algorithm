b2edc26c213508ccad41de8285615428a5cded50
rainbow <562783546@qq.com>
2025-02-21 17:37:57 +0800

    层级遍历

--- src/Tree/TreeNode.java
+++ src/Tree/TreeNode.java
@@ -5,6 +5,13 @@ package Tree;
  * @time 2025-02-21 15:22
  * @description ...
  */
+
+
+/**
+ * 前序遍历即先使用节点，
+ * 后序遍历即后使用节点。
+ * 设计算法时，遍历顺序即节点使用时刻
+ */
 public class TreeNode {
     int val;
     TreeNode left;
