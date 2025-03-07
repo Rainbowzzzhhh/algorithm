b8e8c585e62735ab14d964cf8e55099af8d860f6
rainbow <562783546@qq.com>
2025-03-07 11:02:21 +0800

    55 跳跃游戏2

--- src/main/java/LeekCode/Solution55.java
+++ src/main/java/LeekCode/Solution55.java
@@ -2,26 +2,37 @@ package LeekCode;
 
 /**
  * @author rainbow
- * @time 2025-03-04 09:37
+ * @time 2025-03-07 10:26
  * @description ...
  */
 public class Solution55 {
-    public boolean canJump(int[] nums) {
-        int length = nums.length - 1;
-        if (length == 0) return true;
+    public int jump(int[] nums) {
+        int length = nums.length;
+        if (length == 1) return 0;
+        int path = 1;
+        int maxReachedPlaceNow = nums[0];
+        int maxReachedPlaceLast = 0;
 
-        int maxReachableIndex = nums[0];
-        for (int i = 1; i <= length; i++) {
-            if (i <= maxReachableIndex)
-                maxReachableIndex = Math.max(i + nums[i], maxReachableIndex);
-            if (maxReachableIndex >= length) return true;
+        for (int i = 1; maxReachedPlaceNow < length - 1; i++) {
+            int maxRangeOfi = i + nums[i];
+            if (maxRangeOfi > maxReachedPlaceNow) {
+                if (i <= maxReachedPlaceLast) {
+                    //为上一个范围内更好的下一步，因此不更新步数
+                    maxReachedPlaceNow = maxRangeOfi;
+                } else {
+                    //为新的一步
+                    maxReachedPlaceLast = maxReachedPlaceNow;
+                    maxReachedPlaceNow = maxRangeOfi;
+                    path++;
+                }
+            }
         }
-        return false;
+        return path;
     }
 
     public static void main(String[] args) {
-        int[] a = {2, 3, 1, 1, 4};
-        boolean b = new Solution55().canJump(a);
-        System.out.println(b);
+        int[] nums = {2, 1};
+        int path = new Solution45().jump(nums);
+        System.out.println(path);
     }
 }
